a1b8f43c8577d191daaca9d92ebd062387d081b5
Antoine <end2endzone@gmail.com>
2017-12-17 15:58:57 -0500

    Created dummy foobar c++ project

---
+++ foobar/foobar/foobar.cpp
@@ -0,0 +1,12 @@
+// foobar.cpp : Defines the entry point for the console application.
+//
+
+#include <stdio.h>
+#include <tchar.h>
+
+int main(int argc, char* argv[])
+{
+  printf("FOOBAR!\n");
+	return 0;
+}
+
