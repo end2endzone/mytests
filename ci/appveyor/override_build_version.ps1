5b68908f5e1dec45688a925a264a6c642912bad7
Antoine <end2endzone@gmail.com>
2017-12-17 20:28:33 -0500

    Added build number in appveyor build version to allow building on every commit. If the build number is not part of the version, then appveyor build fails with message "Build version x.y.z already exists." Modified override_build_version.ps1 script to remove build number from product version on master branch.

--- ci/appveyor/override_build_version.ps1
+++ ci/appveyor/override_build_version.ps1
@@ -1,9 +1,17 @@
 Write-Output "Running override_build_version.ps1..."
 Write-Output "Current branch: $env:APPVEYOR_REPO_BRANCH"
+Write-Output "Current build version: $env:APPVEYOR_BUILD_VERSION"
 If ("$env:APPVEYOR_REPO_BRANCH" -eq "master") {
+  $env:APPVEYOR_BUILD_VERSION -match "^(?<major>\d+)(\.(?<minor>\d+))?(\.(?<patch>\d+))?(\-(?<pre>[0-9A-Za-z\-\.]+))?(\+(?<build>[0-9A-Za-z\-\.]+))?$" | Out-Null
+  $major = [int]$matches['major']
+  $minor = [int]$matches['minor']
+  $patch = [int]$matches['patch']
+  
+  #Strip out build number from $env:APPVEYOR_BUILD_VERSION
+  $env:APPVEYOR_BUILD_VERSION = "$major.$minor.$patch"
+  
   $env:GITHUB_TAG_NAME="v$env:APPVEYOR_BUILD_VERSION-win32"
   $env:GITHUB_RELEASE_NAME="$env:APPVEYOR_PROJECT_NAME-v$env:APPVEYOR_BUILD_VERSION-win32"
-  $env:PRODUCT_VERSION="$env:APPVEYOR_BUILD_VERSION"
 }
 Else
 {
@@ -13,4 +21,4 @@ Else
 }
 Write-Output "GITHUB_TAG_NAME=$env:GITHUB_TAG_NAME"
 Write-Output "GITHUB_RELEASE_NAME=$env:GITHUB_RELEASE_NAME"
-Write-Output "PRODUCT_VERSION=$env:PRODUCT_VERSION"
+Write-Output "APPVEYOR_BUILD_VERSION=$env:APPVEYOR_BUILD_VERSION"
