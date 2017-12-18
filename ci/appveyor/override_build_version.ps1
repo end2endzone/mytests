041b71d5768729e8458980d5fcb13c0d2a0a514a
Antoine <end2endzone@gmail.com>
2017-12-18 16:09:23 -0500

    Fixed extracting semantic versioning fields in override_build_version.ps1 (again)

--- ci/appveyor/override_build_version.ps1
+++ ci/appveyor/override_build_version.ps1
@@ -2,7 +2,7 @@ Write-Output "Running override_build_version.ps1..."
 Write-Output "Current branch: $env:APPVEYOR_REPO_BRANCH"
 Write-Output "Current build version: $env:APPVEYOR_BUILD_VERSION"
 If ("$env:APPVEYOR_REPO_BRANCH" -eq "master") {
-  $major, $minor, $patch, $build = $myversion.Split(".")
+  $major, $minor, $patch, $build = $env:APPVEYOR_BUILD_VERSION.Split(".")
   
   #Strip out build number from $env:APPVEYOR_BUILD_VERSION
   $env:APPVEYOR_BUILD_VERSION = "$major.$minor.$patch"
