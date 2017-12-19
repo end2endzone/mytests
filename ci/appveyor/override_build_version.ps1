80bc8fb8d7a4f48bb5a8ab7351ee0b793e000463
Antoine <end2endzone@gmail.com>
2017-12-19 10:50:53 -0500

    Cleaned up override_build_version.ps1

--- ci/appveyor/override_build_version.ps1
+++ ci/appveyor/override_build_version.ps1
@@ -2,20 +2,20 @@ Write-Output "Running override_build_version.ps1..."
 Write-Output "Current branch: $env:APPVEYOR_REPO_BRANCH"
 Write-Output "Current build version: $env:APPVEYOR_BUILD_VERSION"
 If ("$env:APPVEYOR_REPO_BRANCH" -contains "prerelease") {
-  Write-Output "Building from $env:APPVEYOR_REPO_BRANCH branch. Deploying each commits to GitHub..."
+  Write-Output "Tagging each commits with a unique version..."
+  Write-Output "Deploying each commits to GitHub..."
 
   $env:GITHUB_TAG_NAME="v$env:APPVEYOR_BUILD_VERSION-win32"
   $env:GITHUB_RELEASE_NAME="$env:APPVEYOR_PROJECT_NAME-v$env:APPVEYOR_BUILD_VERSION-win32"
 }
 Else
 {
-  Write-Output "Downgrading APPVEYOR_BUILD_VERSION environment variable...."
-
+  #Downgrading APPVEYOR_BUILD_VERSION to x.y.z instead of x.y.z.b
   $major, $minor, $patch, $build = $env:APPVEYOR_BUILD_VERSION.Split(".")
   #Strip out build number from $env:APPVEYOR_BUILD_VERSION
   $env:APPVEYOR_BUILD_VERSION = "$major.$minor.$patch"
-  
-  Write-Output "APPVEYOR_BUILD_VERSION is now set to '$env:APPVEYOR_BUILD_VERSION'"
+
+  Write-Output "Downgrading APPVEYOR_BUILD_VERSION environment variable to '$env:APPVEYOR_BUILD_VERSION'"
   
   $env:GITHUB_TAG_NAME="nightly"
   $env:GITHUB_RELEASE_NAME="Nightly build (branch $env:APPVEYOR_REPO_BRANCH)"
