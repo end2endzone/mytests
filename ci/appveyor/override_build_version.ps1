4a5e01a07a7c00997ea4a2a2152995c82177367b
Antoine <end2endzone@gmail.com>
2017-12-19 10:43:33 -0500

    Modified appveyor.yml: * Now calling /ci/appveyor/build_version_split.ps1 to split each values of version into individual fields. * Removed random value from foobar.exe artifact. Modified override_build_version.ps1: * Now looking for a prerelease branch for deploying each commits to GitHub as version x.y.z.buildnumber * Other branches (master, issue34, anyname) are deployed as tag "nightly" with a release name "Nightly build (branch $env:APPVEYOR_REPO_BRANCH)". Artifacts are released as version x.y.z

--- ci/appveyor/override_build_version.ps1
+++ ci/appveyor/override_build_version.ps1
@@ -1,23 +1,25 @@
 Write-Output "Running override_build_version.ps1..."
 Write-Output "Current branch: $env:APPVEYOR_REPO_BRANCH"
 Write-Output "Current build version: $env:APPVEYOR_BUILD_VERSION"
-If ("$env:APPVEYOR_REPO_BRANCH" -eq "master") {
-  Write-Output "Building from master branch. Downgrading APPVEYOR_PROJECT_NAME environment variable."
+If ("$env:APPVEYOR_REPO_BRANCH" -contains "prerelease") {
+  Write-Output "Building from $env:APPVEYOR_REPO_BRANCH branch. Deploying each commits to GitHub..."
 
-  $major, $minor, $patch, $build = $env:APPVEYOR_BUILD_VERSION.Split(".")
-  
-  #Strip out build number from $env:APPVEYOR_BUILD_VERSION
-  $env:APPVEYOR_BUILD_VERSION = "$major.$minor.$patch"
-  
   $env:GITHUB_TAG_NAME="v$env:APPVEYOR_BUILD_VERSION-win32"
   $env:GITHUB_RELEASE_NAME="$env:APPVEYOR_PROJECT_NAME-v$env:APPVEYOR_BUILD_VERSION-win32"
 }
 Else
 {
-  $env:GITHUB_TAG_NAME="v$env:APPVEYOR_BUILD_VERSION.$env:APPVEYOR_BUILD_NUMBER-win32"
-  $env:GITHUB_RELEASE_NAME="$env:APPVEYOR_PROJECT_NAME-v$env:APPVEYOR_BUILD_VERSION.$env:APPVEYOR_BUILD_NUMBER-win32"
-  $env:PRODUCT_VERSION="$env:APPVEYOR_BUILD_VERSION.$env:APPVEYOR_BUILD_NUMBER"
+  Write-Output "Downgrading APPVEYOR_BUILD_VERSION environment variable...."
+
+  $major, $minor, $patch, $build = $env:APPVEYOR_BUILD_VERSION.Split(".")
+  #Strip out build number from $env:APPVEYOR_BUILD_VERSION
+  $env:APPVEYOR_BUILD_VERSION = "$major.$minor.$patch"
+  
+  Write-Output "APPVEYOR_BUILD_VERSION is now set to '$env:APPVEYOR_BUILD_VERSION'"
+  
+  $env:GITHUB_TAG_NAME="nightly"
+  $env:GITHUB_RELEASE_NAME="Nightly build (branch $env:APPVEYOR_REPO_BRANCH)"
 }
+Write-Output "APPVEYOR_BUILD_VERSION=$env:APPVEYOR_BUILD_VERSION"
 Write-Output "GITHUB_TAG_NAME=$env:GITHUB_TAG_NAME"
 Write-Output "GITHUB_RELEASE_NAME=$env:GITHUB_RELEASE_NAME"
-Write-Output "APPVEYOR_BUILD_VERSION=$env:APPVEYOR_BUILD_VERSION"
