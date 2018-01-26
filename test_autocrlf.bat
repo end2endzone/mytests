87396a15ca5a5efbbb95cdd872da76951cc258e7
Antoine <end2endzone@gmail.com>
2018-01-25 21:28:29 -0500

    Fixed test_autocrlf.bat to not try to upload junit test results as markdown files... dah... Modified appveyor.yml to upload README_*.md files as artifacts.

--- test_autocrlf.bat
+++ test_autocrlf.bat
@@ -12,20 +12,3 @@ git clone https://github.com/end2endzone/veyortests.git test_false
 ren c:\projects\veyortests\test_true\README.md README_true.md
 ren c:\projects\veyortests\test_input\README.md README_input.md
 ren c:\projects\veyortests\test_false\README.md README_false.md
-
-echo =======================================================================
-echo Uploading autocrlf artifacts to AppVeyor
-echo =======================================================================
-set TEST_RESULT_URL=https://ci.appveyor.com/api/testresults/junit/%APPVEYOR_JOB_ID%
-set TEST_RESULT_FILE=c:\projects\veyortests\test_true\README_true.md
-echo TEST_RESULT_URL=%TEST_RESULT_URL%
-echo TEST_RESULT_FILE=%TEST_RESULT_FILE%
-powershell "(New-Object 'System.Net.WebClient').UploadFile($($env:TEST_RESULT_URL), $($env:TEST_RESULT_FILE))"
-set TEST_RESULT_FILE=c:\projects\veyortests\test_true\README_input.md
-echo TEST_RESULT_URL=%TEST_RESULT_URL%
-echo TEST_RESULT_FILE=%TEST_RESULT_FILE%
-powershell "(New-Object 'System.Net.WebClient').UploadFile($($env:TEST_RESULT_URL), $($env:TEST_RESULT_FILE))"
-set TEST_RESULT_FILE=c:\projects\veyortests\test_true\README_false.md
-echo TEST_RESULT_URL=%TEST_RESULT_URL%
-echo TEST_RESULT_FILE=%TEST_RESULT_FILE%
-powershell "(New-Object 'System.Net.WebClient').UploadFile($($env:TEST_RESULT_URL), $($env:TEST_RESULT_FILE))"
