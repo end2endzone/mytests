3f18298e6ee59c4809116f90b5c176517c75a118
Antoine <end2endzone@gmail.com>
2020-08-24 15:36:14 -0400

    Added comment for searching for Wix Toolset.

--- ci/appveyor/prepare_github_deploy.bat
+++ ci/appveyor/prepare_github_deploy.bat
@@ -2,7 +2,7 @@
 set APPVEYOR_PROJECT_NAME=veyortests
 set APPVEYOR_BUILD_NUMBER=9999
 set APPVEYOR_BUILD_VERSION=3.4.5.%APPVEYOR_BUILD_NUMBER%
-set SEMVER_STRING=3.4.5
+set PRODUCT_VERSION=3.4.5
 set APPVEYOR_REPO_BRANCH=prerelease
 powershell -nologo -executionpolicy bypass -File "%~dpn0.ps1"
 pause
