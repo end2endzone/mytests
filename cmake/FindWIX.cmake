f018ac07e254501b24dadf439832bc4b6fd1d827
Antoine <end2endzone@gmail.com>
2020-08-24 21:20:46 -0400

    Replaced '\' by '/'.

--- cmake/FindWIX.cmake
+++ cmake/FindWIX.cmake
@@ -20,16 +20,20 @@ if (WIN32)
   # Define possible search locations in the registry
   set(_WIX_ROOT_HINTS
     "[HKEY_LOCAL_MACHINE\\SOFTWARE\\WOW6432Node\\Microsoft\\Windows Installer XML;InstallFolder")
-
+  
   # Search for directories matching 'WiX Toolset*' in both Program Files directories.
   file(GLOB _WIX_DIRECTORIES_X64 "C:/Program Files/WiX Toolset*")
   file(GLOB _WIX_DIRECTORIES_X86 "C:/Program Files (x86)/WiX Toolset*")
-    
+  
+  # The environment variable WIX can end with a '\' character. This backslash character is escaping the next character which is the list separator ';'.
+  set(_WIX_ENV_PATH "$ENV{WIX}")
+  string(REPLACE "\\" "/" _WIX_ENV_PATH ${_WIX_ENV_PATH})
+  
   # Define know directory locations where it could be installed.
   set(_WIX_ROOT_PATHS
-    "$ENV{WIX}"
-    "${_WIX_DIRECTORIES_X86}"
-    "${_WIX_DIRECTORIES_X64}"
+    ${_WIX_ENV_PATH}
+    ${_WIX_DIRECTORIES_X86}
+    ${_WIX_DIRECTORIES_X64}
   )
   message("_WIX_ROOT_PATHS=" "${_WIX_ROOT_PATHS}")
   
@@ -38,23 +42,23 @@ if (WIN32)
     NAMES
       bin/candle.exe
     HINTS
-      "${_WIX_ROOT_HINTS}"
+      ${_WIX_ROOT_HINTS}
     PATHS
-      "${_WIX_ROOT_PATHS}"
+      ${_WIX_ROOT_PATHS}
   )
   mark_as_advanced(WIX_ROOT_PATH)
   message("WIX_ROOT_PATH=" "${WIX_ROOT_PATH}")
-
+  
   if (WIX_ROOT_PATH)
     set(WIX_BIN_DIR ${WIX_ROOT_PATH}/bin)
   endif (WIX_ROOT_PATH)
-    
+  
   # Search for the main executables
   find_program(WIX_CANDLE
     NAMES
       candle
     PATHS
-      "${WIX_BIN_DIR}"
+      ${WIX_BIN_DIR}
   )
   message("WIX_CANDLE=" "${WIX_CANDLE}")
   mark_as_advanced(WIX_CANDLE)
@@ -62,7 +66,7 @@ if (WIN32)
     NAMES
       light
     PATHS
-      "${WIX_BIN_DIR}"
+      ${WIX_BIN_DIR}
   )
   message("WIX_LIGHT=" "${WIX_LIGHT}")
   mark_as_advanced(WIX_LIGHT)
